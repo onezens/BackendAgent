9fd78e801970e17dc91b45e6069b6ef6969d3c00
王振 <wangzhen@lexue.com>
2017-08-08 19:36:01 +0800

    post

--- config/default.js
+++ config/default.js
@@ -2,7 +2,10 @@ module.exports = {
     port: 8080,
     host: '127.0.0.1',
     agentHosts: [
-        'api.weibo.cn'
+        'api.weibo.cn',
+        'api.baidu.com',
+        'www.hoapi.com',
+        'www.hoapi.com:6000'
     ],
     agentPort: 8888
 };
--- router/index.js
+++ router/index.js
@@ -2,7 +2,14 @@
 var config = require('../config/default');
 var http = require('http');
 var https = require('https');
+var formidable = require('formidable');
+var querystring=require("querystring");
+var zlib = require('zlib');
+
+var url = require('url');
+
 var profileData = require('../json/profile.json');
+
 
 function router(req, res, isHttps, next) {
     if (req.method == 'GET'){
@@ -30,7 +37,14 @@ function router(req, res, isHttps, next) {
             fetchData(netSpider, agentReqUrl, res);
         }
 
+    }else if (req.method == 'POST'){
+        var form = new formidable.IncomingForm();
+
+        form.parse(req, function(err, fields, files) {
+            postData(req, res, fields, next);
+        });
     }else{
+        console.log('Unsupported request method : ' + req.method);
         next();
     }
 }
@@ -44,6 +58,7 @@ function fetchData(netSpider,agentReqUrl,res) {
             });
             res2.on('end', function () {
                 logData(agentReqUrl, data);
+
                 res.writeHead(200, 'success',{
                     'content-type': 'text/plain;charset=UTF-8'
                 });
@@ -56,6 +71,72 @@ function fetchData(netSpider,agentReqUrl,res) {
         res.end(err.toString());
         console.log('Get Data Error: ' + agentReqUrl);
     }
+}
+
+
+function postData(reqOri, resOri, fields, next) {
+    var headers = reqOri.headers;
+    var host = headers.host;
+    var urlObj = url.parse(reqOri.url);
+    var path = urlObj.path;
+    var defaultPort = reqOri.url.indexOf('http')!=-1 ? 80: 443;
+    var port = urlObj.port ? urlObj.port : defaultPort;
+    var postData=querystring.stringify(fields);
+    var options={
+        hostname:host,
+        port:port,
+        path:path,
+        method:"POST",
+        headers:headers
+    }
+
+    var req=http.request(options,function(res){
+
+
+        if(res.headers['content-encoding'].indexOf('gzip') != -1){
+            var resDataGzip = [];
+            res.on("data",function(chunk){
+                resDataGzip.push(chunk);
+            });
+            res.on("end",function(){
+                var buffer = Buffer.concat(resDataGzip);
+                zlib.gunzip(buffer, function(err, decoded) {
+                    resEnd(resOri, decoded.toString(), res.headers);
+                })
+            });
+
+        }else{
+            var resData = '';
+            res.on("data",function(chunk){
+                resData += chunk;
+            });
+            res.on("end",function(){
+
+                resEnd(resOri, resData, res.headers);
+
+            });
+        }
+
+
+
+
+
+    });
+
+    req.on("error",function(err){
+        console.log(err.message);
+        next();
+    })
+    req.write(postData);
+    req.end();
+
+}
+
+function resEnd(res ,endData, headers) {
+    console.log(endData);
+    res.writeHead(200, 'success',headers);
+    res.write(endData, 'utf8');
+    res.end();
 }
 
 
